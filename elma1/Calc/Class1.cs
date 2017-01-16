2ffd015b3de996c3c778cb1620d7f6e544c7b259
shuro4ka <shurka1024@gmail.com>
2017-01-16 21:06:15 +0400

    Добавлено еще три операции: с двумя, одной и без переменных

--- elma1/Calc/Class1.cs
+++ elma1/Calc/Class1.cs
@@ -13,19 +13,33 @@ namespace Calc
             return (int)Execute("Sum", new object[] { x, y });
         }
 
+
         public Calc(IOperation[] opers)     // Конструктор
         {
             operations = opers;                     // Указываем список операций
         }
 
+        public Calc(IOperationEmpty[] opers)     // Конструктор
+        {
+            operations_empty = opers;                     // Указываем список операций
+        }
+
         // Список всех операций
         private IOperation[] operations { get; set; }
+        private IOperationEmpty[] operations_empty { get; set; }
 
         public object Execute(string name, object[] args)
         {
             // Выбирем первую операцию с именем Name
-            var oper = operations.FirstOrDefault(o => o.Name == name);
+            var oper = operations.FirstOrDefault(oe => oe.Name == name);
             return oper.Execute(args);  // Выполняется метод с именем Name
+        }
+
+        public object ExecuteEmpty(string name)
+        {
+            // Выбирем первую операцию с именем Name
+            var oper = operations_empty.FirstOrDefault(oe => oe.Name == name);
+            return oper.Execute();  // Выполняется метод с именем Name
         }
 
     }
@@ -36,12 +50,49 @@ namespace Calc
         object Execute(object[] args);
     }
 
+    public interface IOperationEmpty
+    {
+        string Name { get; }
+        object Execute();
+    }
+
+    // Реализация метода "Сложение"
     public class SumOperation : IOperation
     {
         public string Name { get { return "Sum"; } }
         public object Execute(object[] args)
         {
             return (int)args[0] + (int)args[1];
+        }
+    }
+
+    // Реализация метода "Возведение в степень"
+    public class PowerOperation : IOperation
+    {
+        public string Name { get { return "Power"; } }
+        public object Execute(object[] args)
+        {
+            return System.Math.Pow(Convert.ToDouble(args[0]), Convert.ToDouble(args[1]));
+        }
+    }
+
+    // Реализация метода "Инкрементация"
+    public class IncOperation : IOperation
+    {
+        public string Name { get { return "Inc"; } }
+        public object Execute(object[] args)
+        {
+            return (int)args[0] + 1;
+        }
+    }
+
+    // Реализация метода "Короткое число Пи"
+    public class ShotPiOperation : IOperationEmpty
+    {
+        public string Name { get { return "ShotPi"; } }
+        public object Execute()
+        {
+            return 3.14;
         }
     }
 }
--- elma1/UnitTestProject1/UnitTest1.cs
+++ elma1/UnitTestProject1/UnitTest1.cs
@@ -13,9 +13,33 @@ namespace UnitTestProject1
         [TestMethod]        // Метод тестер
         public void SumTest()
         {
-            var calc = new C.Calc(new Calc.IOperation[] { new Calc.SumOperation() });          // Объявление экземпляра класса
-            var result = calc.Execute("Sum", new object[] { 1, 2 });
-            Assert.AreEqual(result, 3);
+            var calc = new C.Calc(new C.IOperation[] { new C.SumOperation() });          // Объявление экземпляра класса
+            var result_sum = calc.Execute("Sum", new object[] { 1, 2 });
+            Assert.AreEqual(result_sum, 3);
+        }
+
+        [TestMethod]
+        public void PowerTest()
+        {
+            var calc = new C.Calc(new C.IOperation[] { new C.PowerOperation() });          // Объявление экземпляра класса
+            var result = calc.Execute("Power", new object[] { 2, 3 });
+            Assert.AreEqual(result, (double)8);
+        }
+
+        [TestMethod]
+        public void IncTest()
+        {
+            var calc = new C.Calc(new C.IOperation[] { new C.IncOperation() });
+            var result = calc.Execute("Inc", new object[] { 4 });
+            Assert.AreEqual(result, 5);
+        }
+
+        [TestMethod]
+        public void ShotPiTest()
+        {
+            var calc = new C.Calc(new C.IOperationEmpty[] { new C.ShotPiOperation() });
+            var result = calc.ExecuteEmpty("ShotPi");
+            Assert.AreEqual(result, 3.14);
         }
     }
 }
